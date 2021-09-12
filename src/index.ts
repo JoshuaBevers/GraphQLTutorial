88d37c39d213bc0af0e78b72b49353fa6014d6ee
infernezor@gmail.com <infernezor@gmail.com>
2021-09-11 23:13:08 -0400

    Mikro setup.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: 'lireddit',
-    type: 'postgresql',
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroConfig);
+  orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: 'my first post' });
+  await orm.em.persistAndFlush(post);
 };
+console.log(__dirname);
+main().catch((err) => {
+  console.log(err);
+});
 
 console.log('hello there');
